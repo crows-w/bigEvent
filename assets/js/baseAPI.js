cf61b8fa7740d57f0611e6f1eb64a4bd77a749cf
crows <944303139@qq.com>
2020-09-11 11:25:38 +0800

    完成主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,20 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
     console.log(options.url);
+    // 统一有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 不论成功还是失败都会调用 complete 回调函数
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 强制清空 token
+            localStorage.removeItem('token')
+            // 强制跳转登录页面
+            location.href = '/login.html'
+        }
+    }
 })
